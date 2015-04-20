17183d6a9f8da9879db80c74874107eb39e66318
Juan Garcia <juan.garcia@synergy-gb.com>
2015-04-20 14:35:08 -0500

    Solventando error en método llamado por ng-change en la plantilla HTML

--- src/sgb-screen-list.js
+++ src/sgb-screen-list.js
@@ -4,7 +4,7 @@ angular.module('sgb-screen-list', ['megazord'])
   .controller('sgb-screen-list-controller', ['$stateParams', '$scope', function($stateParams, $scope){
     $scope.items = $stateParams.data;
 
-    $scope.itemClickHandler = function(){};
+    $scope.itemSearchHandler = function(){};
 
     $scope.itemSearchCancelHandler = function(){};
 
