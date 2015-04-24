0dc94e9a7af77e3b2abf886a76f0c24909890b6d
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-24 17:55:05 -0430

    Title can now be configured

--- dist/sgb-screen-list.js
+++ dist/sgb-screen-list.js
@@ -2,14 +2,15 @@
 'use strict';
 
 angular.module('sgb-screen-list', ['megazord'])
-  .controller('sgb-screen-list-controller', ['_router','$stateParams', '$scope', function(_router, $stateParams, $scope){
-    $scope.items = $stateParams.data;
+  .controller('sgb-screen-list-controller', ['_router', '_screenParams','$stateParams', '$scope', function(_router, _screenParams, $stateParams, $scope){
+      $scope.items = $stateParams.data;
+      $scope.title = _screenParams.title;
 
-    $scope.itemSearchHandler = function(){};
+      $scope.itemSearchHandler = function(){};
 
-    $scope.itemSearchCancelHandler = function(){};
+      $scope.itemSearchCancelHandler = function(){};
 
-    $scope.itemClickHandler = function(item){
+      $scope.itemClickHandler = function(item){
       //Nothing to do but fire the event
       console.log('Going to fire event with ' + item);
       _router.fireEvent({
