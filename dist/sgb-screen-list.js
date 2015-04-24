f555c88a5f8fdc45714d9f5c74f7f87c28b30121
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-24 16:45:08 -0430

    Added event firing

--- dist/sgb-screen-list.js
+++ dist/sgb-screen-list.js
@@ -2,14 +2,22 @@
 'use strict';
 
 angular.module('sgb-screen-list', ['megazord'])
-  .controller('sgb-screen-list-controller', ['$stateParams', '$scope', function($stateParams, $scope){
+  .controller('sgb-screen-list-controller', ['_router','$stateParams', '$scope', function(_router, $stateParams, $scope){
     $scope.items = $stateParams.data;
 
     $scope.itemSearchHandler = function(){};
 
     $scope.itemSearchCancelHandler = function(){};
 
-    $scope.itemClickHandler = function(){};
+    $scope.itemClickHandler = function(item){
+      //Nothing to do but fire the event
+      _router.fireEvent({
+        name: 'item_click',
+        params: {
+          item: item
+        }
+      })
+    };
 
   }]);
 },{}]},{},[1]);
