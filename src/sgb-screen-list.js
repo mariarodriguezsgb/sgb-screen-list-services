074d6b310f9aba4ce3b4652e6b1e0ef3729e3c6b
Saul Hernandez <sauldhernandez@gmail.com>
2015-04-27 17:37:11 -0430

    Test

--- src/sgb-screen-list.js
+++ src/sgb-screen-list.js
@@ -9,8 +9,8 @@ angular.module('sgb-screen-list', ['megazord'])
         $scope.title = _screenParams.title || 'list_title';
 
         $scope.filterItems = function(){
-            console.log('scope is ' +$scope.searchText);
-            var search = $scope.searchText.toLowerCase();
+            console.log('scope is ' + $scope.searchQuery);
+            var search = $scope.searchQuery.toLowerCase();
             $scope.filteredItems = _.filter($scope.items, function(item){
                 return (item.title && item.title.toLowerCase().indexOf(search) != -1) ||
                     (item.detail1 && item.detail1.toLowerCase().indexOf(search) != -1) ||
